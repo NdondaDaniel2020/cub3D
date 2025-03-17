8d96319d1db28233f0148731b1af6bedb1480a88
Angelo Quissanga <aquissan@c1r6s4.42luanda.com>
2025-03-17 15:12:43 +0100

    Multiples errors messages bug solved

--- sources/check_map.c
+++ sources/check_map.c
@@ -6,7 +6,7 @@
 /*   By: aquissan <aquissan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 08:42:09 by aquissan          #+#    #+#             */
-/*   Updated: 2025/03/15 07:02:18 by aquissan         ###   ########.fr       */
+/*   Updated: 2025/03/17 14:43:36 by aquissan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,6 @@ int	get_campus(t_map *map, t_master **master)
 	}
 	else
 		(*master)->campus = NULL;
-	(*master)->wrongmap = 1;
 	return (1);
 }
 
@@ -141,6 +140,8 @@ t_master	*get_master(t_map *map)
 		}
 		tmp = tmp->next;
 	}
+	if (master->wrongmap)
+		return (master);
 	is_there_something_wrong(master, tmp);
 	if (master->campus)
 		check_campus(master);
