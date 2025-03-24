b52130761ac2163739c150e84348eb866abdcb8e
Angelo Quissanga <aquissan@c1r6s4.42luanda.com>
2025-03-24 12:54:23 +0100

    Vertical rotation done

--- source_bonus/draw_straight_line_bonus.c
+++ source_bonus/draw_straight_line_bonus.c
@@ -3,57 +3,59 @@
 /*                                                        :::      ::::::::   */
 /*   draw_straight_line_bonus.c                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nmatondo <nmatondo@student.42.fr>          #+#  +:+       +#+        */
+/*   By: aquissan <aquissan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-03-20 12:10:04 by nmatondo          #+#    #+#             */
-/*   Updated: 2025-03-20 12:10:04 by nmatondo         ###   ########.fr       */
+/*   Created: 2025/03/20 12:10:04 by nmatondo          #+#    #+#             */
+/*   Updated: 2025/03/24 12:38:49 by aquissan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3D_bonus.h"
 
-void	draw_big_pixel(t_master *master, int size, int x, int y, int color) /* falha */
+void	draw_big_pixel(t_master *master, int size, t_intvector vet, int color)
 {
-	int i;
-	int j;
+	int	i;
+	int	j;
 
 	i = 0;
-    while (i < size)
-    {
+	while (i < size)
+	{
 		j = 0;
-        while (j < size)
-        {
-			my_mlx_pixel_put(&master->img, x + i, y + j, color);
+		while (j < size)
+		{
+			my_mlx_pixel_put(&master->img, vet.x + i, vet.y + j, color);
 			j++;
-        }
+		}
 		i++;
-    }
+	}
 }
 
 void	init_bresenham(t_bresenham *bresenham)
 {
-    bresenham->dx = bresenham->x_end - bresenham->x_ini;
-    bresenham->dy = bresenham->y_end - bresenham->y_ini;
-    bresenham->x = bresenham->x_ini;
-    bresenham->y = bresenham->y_ini;
+	bresenham->dx = bresenham->x_end - bresenham->x_ini;
+	bresenham->dy = bresenham->y_end - bresenham->y_ini;
+	bresenham->x = bresenham->x_ini;
+	bresenham->y = bresenham->y_ini;
 	bresenham->inc_x = -1;
 	bresenham->inc_y = -1;
 	if (bresenham->dx > 0)
-    	bresenham->inc_x = 1;
+		bresenham->inc_x = 1;
 	if (bresenham->dy > 0)
-    	bresenham->inc_y = 1;
-    bresenham->dx = abs(bresenham->dx);
-    bresenham->dy = abs(bresenham->dy);
+		bresenham->inc_y = 1;
+	bresenham->dx = abs(bresenham->dx);
+	bresenham->dy = abs(bresenham->dy);
 }
 
-void	draw_positive_straight_line(t_master *master, t_bresenham *bresenham, int size, int color)
+void	draw_positive_straight_line(t_master *master, t_bresenham *bresenham,
+		int size, int color)
 {
 	int	p;
 
 	p = 2 * bresenham->dy - bresenham->dx;
 	while (bresenham->x != bresenham->x_end)
 	{
-		draw_big_pixel(master, size, bresenham->x, bresenham->y, color);
+		draw_big_pixel(master, size, (t_intvector){bresenham->x, bresenham->y},
+			color);
 		bresenham->x += bresenham->inc_x;
 		if (p >= 0)
 		{
@@ -64,14 +66,16 @@ void	draw_positive_straight_line(t_master *master, t_bresenham *bresenham, int s
 	}
 }
 
-void	draw_negative_straight_line(t_master *master, t_bresenham *bresenham, int size, int color)
+void	draw_negative_straight_line(t_master *master, t_bresenham *bresenham,
+		int size, int color)
 {
 	int	p;
 
 	p = 2 * bresenham->dx - bresenham->dy;
 	while (bresenham->y != bresenham->y_end)
 	{
-		draw_big_pixel(master, size, bresenham->x, bresenham->y, color);
+		draw_big_pixel(master, size, (t_intvector){bresenham->x, bresenham->y},
+			color);
 		bresenham->y += bresenham->inc_y;
 		if (p >= 0)
 		{
@@ -82,12 +86,13 @@ void	draw_negative_straight_line(t_master *master, t_bresenham *bresenham, int s
 	}
 }
 
-void	draw_straight_line(t_master *master, t_bresenham *line, int size, int color)
+void	draw_straight_line(t_master *master, t_bresenham *line, int size,
+		int color)
 {
 	init_bresenham(line);
-    if (line->dx > line->dy)
+	if (line->dx > line->dy)
 		draw_positive_straight_line(master, line, size, color);
-    else
+	else
 		draw_negative_straight_line(master, line, size, color);
-	draw_big_pixel(master, size, line->x, line->y, color);
+	draw_big_pixel(master, size, (t_intvector){line->x, line->y}, color);
 }
--- source_bonus/mouse_event_bonus.c
+++ source_bonus/mouse_event_bonus.c
@@ -6,19 +6,31 @@
 /*   By: aquissan <aquissan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 12:13:43 by aquissan          #+#    #+#             */
-/*   Updated: 2025/03/21 13:43:27 by aquissan         ###   ########.fr       */
+/*   Updated: 2025/03/24 12:18:39 by aquissan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3D_bonus.h"
 
+int	rotate_up(t_master *master, int div)
+{
+	master->view_high += (SPEED * ROTATE_SPEED) / div;
+	return (0);
+}
+
+int	rotate_down(t_master *master, int div)
+{
+	master->view_high -= (SPEED * ROTATE_SPEED) / div;
+	return (0);
+}
+
 int	mousemove(int x, int y, t_master *master)
 {
 	int			div;
 	static int	posx;
+	static int	posy;
 
 	div = 4;
-	(void)y;
 	if (posx > x)
 	{
 		master->render.dir = rotate_vector(master->render.dir, (SPEED / div));
@@ -31,6 +43,11 @@ int	mousemove(int x, int y, t_master *master)
 		master->render.plane = rotate_vector(master->render.plane, -(SPEED
 					/ div));
 	}
+	if (posy > y)
+		rotate_up(master, div);
+	if (posy < y)
+		rotate_down(master, div);
 	posx = x;
+	posy = y;
 	return (0);
 }
