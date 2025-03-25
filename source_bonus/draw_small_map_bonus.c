09c4cf8bf61c47d0cfd2e62414a6839a5e51f3db
Ndonda Matondo <nmatondo@c2r11s1.42luanda.com>
2025-03-25 10:54:09 +0100

    add draw_smal_map
    Calculo da area do mini mapa feito.

--- source_bonus/draw_small_map_bonus.c
+++ source_bonus/draw_small_map_bonus.c
@@ -11,39 +11,7 @@
 /* ************************************************************************** */
 
 #include "../includes/cub3D_bonus.h"
-#define PIXEL_SIZE 50
-
-void	draw_map(t_master *master, t_area *area)
-{
-	int i;
-	int x;
-	int y;
-
-	i = 0;
-	y = 1;
-	while (area->ini_x <= area->end_x)
-	{
-		x = 1;
-		i = area->ini_y;
-		while (i <= area->end_y)
-		{
-			if (master->campus[area->ini_x][i] == '1')
-			{draw_big_pixel(master, PIXEL_SIZE, x * PIXEL_SIZE, y * PIXEL_SIZE, 0x52be80);}
-
-			else if (master->campus[area->ini_x][i] != '1')
-			{draw_big_pixel(master, PIXEL_SIZE, x * PIXEL_SIZE, y * PIXEL_SIZE, 0x3c3c3c);}
-
-			if ((int)master->render.pos.x == area->ini_x && (int)master->render.pos.y == i)
-			{draw_big_pixel(master, PIXEL_SIZE, x * PIXEL_SIZE, y * PIXEL_SIZE, 0xFF0000);}
-
-			i++;
-			x++;
-		}
-		area->ini_x++;
-		y++;
-	}
-	printf("[%f, %f]\n", master->render.pos.x, master->render.pos.y);
-}
+#define PIXEL_SIZE 15
 
 void	draw_small_map(t_master *master)
 {
@@ -53,16 +21,10 @@ void	draw_small_map(t_master *master)
 
 	size.height = get_height_player_line(master) - 1;
 	size.width = get_width_player_line(master) - 1;
-
-	////////////////////////////////////////////////////////////// set area ==
 	area = (t_area){0, 0, 0, 0};
 	extra = (t_area){0, 0, 0, 0};
 	set_area_value(master, &size, &area, &extra);
-
-	////////////////////////////////////////////////////////////// adjust area
 	adjust_initial_area_values(master, &area, &extra);
 	adjust_final_area_values(master, &area, &extra);
 
-	////////////////////////////////////////////////////////////// draw map
-	draw_map(master, &area);
 }
