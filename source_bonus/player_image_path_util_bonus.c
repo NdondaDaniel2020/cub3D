0746be4efd8e79aac53f0cdd4fb873d86b7c04ce
Ndonda Matondo <nmatondo@c2r11s1.42luanda.com>
2025-04-04 13:42:51 +0100

    load_door_texture add no load_all_texteres

--- source_bonus/player_image_path_util_bonus.c
+++ source_bonus/player_image_path_util_bonus.c
@@ -6,7 +6,7 @@
 /*   By: nmatondo <nmatondo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 17:10:10 by nmatondo          #+#    #+#             */
-/*   Updated: 2025/03/29 17:34:26 by nmatondo         ###   ########.fr       */
+/*   Updated: 2025/04/04 08:51:44 by nmatondo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	load_all_texteres(void *mlx, t_data *img, t_master *master)
 {
 	return (load_textures(mlx, img, master)
-		|| load_player_texture(mlx, img));
+		|| load_player_texture(mlx, img) || load_door_texture(mlx, img));
 }
 
 int	get_image_quantity_by_weapon_type(int index)
