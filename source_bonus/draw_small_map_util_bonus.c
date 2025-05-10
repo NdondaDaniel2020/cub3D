d63a14880bb5d8cb24edad8204f384f135f02823
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2025-05-10 20:47:56 +0100

    Falha do mapa resolvido

--- source_bonus/draw_small_map_util_bonus.c
+++ source_bonus/draw_small_map_util_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   draw_small_map_util_bonus.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nmatondo <nmatondo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 12:10:04 by nmatondo          #+#    #+#             */
-/*   Updated: 2025/04/16 13:34:56 by nmatondo         ###   ########.fr       */
+/*   Updated: 2025/05/10 19:54:38 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,14 @@
 
 void	draw_map_util(t_master *master, t_vector pos, int i1, int i2)
 {
-	if (master->campus[i1][i2] == '1')
+	int len_matrix;
+
+	len_matrix = 0;
+	while (master->campus[len_matrix])
+		len_matrix++;
+	if (i1 < 0 || i2 < 0 || i1 >= len_matrix || i2 >= (int)ft_strlen(master->campus[i1]))
+		draw_big_pixel(master, PIXEL_SIZE, pos, 0x3c3c3c);
+	else if (master->campus[i1][i2] == '1')
 		draw_big_pixel(master, PIXEL_SIZE, pos, 0x52be80);
 	else if (master->campus[i1][i2] == 'D' || master->campus[i1][i2] == 'F'
 			|| master->campus[i1][i2] == 'f' || master->campus[i1][i2] == 'G'
