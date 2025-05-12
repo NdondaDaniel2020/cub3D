4f0c548376002a1202fc459a6d65a79d954cc114
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2025-05-12 11:55:47 +0100

    Ajuste na norma

--- source_bonus/draw_small_map_util_bonus.c
+++ source_bonus/draw_small_map_util_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   draw_small_map_util_bonus.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: nmatondo <nmatondo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 12:10:04 by nmatondo          #+#    #+#             */
-/*   Updated: 2025/05/10 19:54:38 by marvin           ###   ########.fr       */
+/*   Updated: 2025/05/12 11:50:55 by nmatondo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 
 void	draw_map_util(t_master *master, t_vector pos, int i1, int i2)
 {
-	int len_matrix;
+	int	len_matrix;
 
 	len_matrix = 0;
 	while (master->campus[len_matrix])
 		len_matrix++;
-	if (i1 < 0 || i2 < 0 || i1 >= len_matrix || i2 >= (int)ft_strlen(master->campus[i1]))
+	if (i1 < 0 || i2 < 0 || i1 >= len_matrix
+		|| i2 >= (int)ft_strlen(master->campus[i1]))
 		draw_big_pixel(master, PIXEL_SIZE, pos, 0x3c3c3c);
 	else if (master->campus[i1][i2] == '1')
 		draw_big_pixel(master, PIXEL_SIZE, pos, 0x52be80);
